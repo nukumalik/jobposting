21be625c1a87e42fa6aa64aca8e52c6fc03e3dad
nukumalik <nukumalik@gmail.com>
2019-10-22 18:51:50 +0700

    Add token authentication

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,17 @@
 const router = require('express').Router();
+const passport = require('passport');
 
+// User Controllers
 const usersController = require('../controllers/usersController');
+
+// Passport Authenticate
+const isAuthenticate = passport.authenticate('jwt', { session: false });
 
 router
 	.get('/', usersController.getUsers)
 	.post('/login', usersController.loginUsers)
 	.post('/register', usersController.registerUsers)
-	.patch('/:id', usersController.updateUsers)
-	.delete('/:id', usersController.deleteUsers);
+	.patch('/:id', isAuthenticate, usersController.updateUsers)
+	.delete('/:id', isAuthenticate, usersController.deleteUsers);
 
 module.exports = router;
